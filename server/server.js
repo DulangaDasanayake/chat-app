3ca014e74cccfb785f438ff387d127e975e4d379
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-11-14 19:07:47 +0530

    server.js basics

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// WebSocket connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for chat messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast to all clients
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
